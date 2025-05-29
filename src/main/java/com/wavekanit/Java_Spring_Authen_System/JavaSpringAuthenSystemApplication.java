ce7a54c22b2c83cf0f5db9443f7760c5b9cd55a5
wavekanit <wavekanit@hotmail.com>
2025-05-30 01:56:50 +0700

    Initial Commit

---
+++ src/main/java/com/wavekanit/Java_Spring_Authen_System/JavaSpringAuthenSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wavekanit.Java_Spring_Authen_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAuthenSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringAuthenSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wavekanit/Java_Spring_Authen_System/JavaSpringAuthenSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wavekanit.Java_Spring_Authen_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAuthenSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
