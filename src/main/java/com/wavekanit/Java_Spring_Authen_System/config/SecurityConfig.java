36d3d64e4a98cd1b076fa5fb348651816cdf1b54
kanitbub <kanitbub@ais.co.th>
2025-06-09 10:16:24 +0700

    fix: fix the warning

--- src/main/java/com/wavekanit/Java_Spring_Authen_System/config/SecurityConfig.java
+++ src/main/java/com/wavekanit/Java_Spring_Authen_System/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +14,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // ✅ disable csrf
-                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // ✅ for iframe (h2 UI)
+                .csrf(AbstractHttpConfigurer::disable) // ✅ disable csrf
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // ✅ for iframe (h2 UI)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/h2-console/**",    // ✅ allow h2 console
