17df38039fe8b1568ef004c7e1750ffa889fa09a
kanitbub <kanitbub@ais.co.th>
2025-06-09 09:26:25 +0700

    fix: implement lombok

--- src/main/java/com/wavekanit/Java_Spring_Authen_System/model/UserModel.java
+++ src/main/java/com/wavekanit/Java_Spring_Authen_System/model/UserModel.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,8 +27,8 @@ public class UserModel {
     public UserModel(UserRegisterRequest payload) {
         this.username = payload.getUsername();
         this.password = payload.getPassword();
-        this.fName = payload.getfName();
-        this.lName = payload.getlName();
+        this.fName = payload.getFName();
+        this.lName = payload.getFName();
         this.email = payload.getEmail();
         this.phone = payload.getPhone();
     }
@@ -35,58 +39,6 @@ public class UserModel {
         this.fName = fName;
         this.lName = lName;
         this.email = email;
-        this.phone = phone;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
         this.phone = phone;
     }
 }
