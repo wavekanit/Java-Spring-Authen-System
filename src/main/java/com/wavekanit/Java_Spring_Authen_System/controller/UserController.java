fc5fd573b262b9c68d4e2c2ff585620da9d63b5c
wavekanit <wavekanit@hotmail.com>
2025-06-09 11:24:14 +0700

    feat: interface for userController and userService

--- src/main/java/com/wavekanit/Java_Spring_Authen_System/controller/UserController.java
+++ src/main/java/com/wavekanit/Java_Spring_Authen_System/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.wavekanit.Java_Spring_Authen_System.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wavekanit.Java_Spring_Authen_System.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public String getUserByUsername(@PathVariable Long id) {
+        return id + "accessed";
+    }
 }
---
+++ src/main/java/com/wavekanit/Java_Spring_Authen_System/service/UserService.java
@@ -0,0 +1,7 @@
+package com.wavekanit.Java_Spring_Authen_System.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
